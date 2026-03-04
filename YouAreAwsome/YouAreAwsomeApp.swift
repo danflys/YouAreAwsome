e6f6fcb85c0c39470782e1094f1236c17bf94e60
danflys <67918234+danflys@users.noreply.github.com>
2026-03-03 20:01:29 -0500

    Initial Commit

---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Daniel Chayes on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
